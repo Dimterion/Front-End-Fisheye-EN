7d95fd95cd1e0c51c3f92d426a3a5e8c742ff6b7
Dmitrii <dimterion59gmail.com>
2022-01-20 16:56:56 +0100

    Added form validation and submission functionality

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,28 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+//Function to close form by ESC key
+window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+});
+
+//Functions for the form submission and logging results to the console
+const formField = document.querySelector("form");
+
+function logToConsole() {
+    const element = formField.elements;
+    console.log ("First name: ", element.firstName.value);
+    console.log ("Last name: ", element.lastName.value);
+    console.log ("Email: ", element.email.value);
+    console.log ("Message: ", element.message.value);
+}
+
+formField.addEventListener("submit", (e) => {
+    e.preventDefault();
+    logToConsole();
+    formField.reset();
+    closeModal();
+  });
