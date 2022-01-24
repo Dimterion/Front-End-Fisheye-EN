9659eb24522689f67ebde8c11ea5fb579bbf34c2
Dmitrii <dimterion59gmail.com>
2022-01-24 16:36:47 +0100

    Started closeup view section

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -80,13 +80,16 @@ function mediaFactory(data) {
       img.setAttribute("src", pictures);
       img.setAttribute("alt", `${title}`);
       img.setAttribute("id", `${id}`);
+      img.setAttribute("class", "gallery-item");
+      img.setAttribute("type", "photo");
     }
 
     if(video !== undefined) {
       img = document.createElement( 'video' );
       img.setAttribute("src", videos);
-      img.setAttribute("type", "video/mp4");
+      img.setAttribute("type", "video");
       img.setAttribute("id", `${id}`);
+      img.setAttribute("class", "gallery-item");
     }
 
     img.setAttribute("onclick", "displayLightBox()");
@@ -131,21 +134,47 @@ async function displayMedia(media, photographers) {
     headerSection.appendChild(photoCardDOM);
   });
 
-  console.log(media);
-  
+  /*  LIGHTBOX-IMAGES-VIEW-START
   let lightBoxItem;
   const lightBox = document.querySelector('.lightbox');
+  const galleryItem = document.querySelectorAll('.gallery-item');
+  let arr = [];
   
-  for (let i = 0; i < media.length; i++) {
-    if (media[i].image !== undefined) {
-      lightBoxItem = document.createElement( 'img' );
-      lightBoxItem.setAttribute("src", `assets/images/individual/${media[i].photographerId}/${media[i].image}`);
-    } else if (media[i].image == undefined) {
-      lightBoxItem = document.createElement( 'video' );
-      lightBoxItem.setAttribute("src", `assets/images/individual/${media[i].photographerId}/${media[i].video}`);
+  //console.log(galleryItem);
+  //console.log(media);
+
+  for (let j = 0; j < galleryItem.length; j++) {
+    for (let i = 0; i < media.length; i++) {
+      if (media[i].image !== undefined && galleryItem[j].id == media[i].id) {
+        lightBoxItem = document.createElement( 'img' );
+        lightBoxItem.setAttribute("src", `assets/images/individual/${media[i].photographerId}/${media[i].image}`);
+        lightBoxItem.setAttribute("id", `${media[i].id}`);
+        lightBox.appendChild(lightBoxItem);
+        arr.push(lightBoxItem);
+      } else if (media[i].image == undefined && galleryItem[j].id == media[i].id) {
+        lightBoxItem = document.createElement( 'video' );
+        lightBoxItem.setAttribute("src", `assets/images/individual/${media[i].photographerId}/${media[i].video}`);
+        lightBoxItem.setAttribute("id", `${media[i].id}`);
+        lightBox.appendChild(lightBoxItem);
+        arr.push(lightBoxItem);
+      }
     }
-    console.log(lightBoxItem.src);
+    //console.log(`${galleryItem[j].id} / ${lightBoxItem.id}`);
   }
+
+  galleryItem.forEach((l) => {
+    console.log(`${l.id} / ${lightBoxItem.id}`);
+    if (l.id == lightBoxItem.id) {
+      lightBoxItem.setAttribute("style", "display: block");
+    } else {
+      lightBoxItem.setAttribute("style", "display: none");
+    }
+  });
+
+  console.log(arr);
+  console.log(lightBox);
+  LIGHTBOX-IMAGES-VIEW-END */
+
 };
 
 //Function to sum up previous ones
@@ -158,6 +187,42 @@ async function initMedia() {
   const filterPhoto = photographers.filter((item) => item.id == idAccess);
 
   displayMedia(filterMedia, filterPhoto);
+
+  //Gallery display
+  const galleryItem = document.querySelectorAll('.gallery-item');
+  const lightBoxModal = document.querySelector('#modal-lightbox');
+  const lightBox = document.querySelector('.lightbox');
+  
+  let lightBoxPhoto = document.createElement( 'img' );
+  let lightBoxVideo = document.createElement( 'video' );
+
+  lightBox.appendChild(lightBoxPhoto);
+  lightBox.appendChild(lightBoxVideo);
+
+  window.onload = () => {
+    for (let k = 0; k < galleryItem.length; k++) {
+      galleryItem[k].onclick = () => {
+        //console.log([k]);
+        function preview () {
+          let selectedImgUrl = galleryItem[k].src;
+          lightBoxPhoto.src = selectedImgUrl;
+          lightBoxVideo.src = selectedImgUrl;
+          //console.log(selectedImgUrl);
+          //console.log(galleryItem[k].getAttribute('type'));
+          if(galleryItem[k].getAttribute('type') == 'photo') {
+            lightBoxPhoto.style.display = "block";
+            lightBoxVideo.style.display = "none";
+          } else {
+            lightBoxVideo.style.display = "block";
+            lightBoxPhoto.style.display = "none";
+          }
+        }
+        preview();
+        lightBoxModal.style.display = "block";
+      }
+    }
+  }
+
 };
 
 initMedia();
